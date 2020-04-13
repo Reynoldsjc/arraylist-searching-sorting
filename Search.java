e8b6731e75d1eaf0e6e43ce6016b61aa3e21f0e7
Reynoldsjc <jackson.c.reynolds22@gmail.com>
2020-04-13 12:09:03 -0600

    finish Searching

---
+++ Search.java
@@ -0,0 +1,53 @@
+import java.util.ArrayList;
+
+public class Search{
+
+
+    public static void main(String []args){
+
+    ArrayList<String> haystack = new ArrayList<String>();
+    String needle;
+    haystack.add("hay");
+    haystack.add("hay");
+    haystack.add("hay");
+    haystack.add("needle");
+    haystack.add("hay");
+    System.out.println(normalSearch(haystack,"needle"));
+    System.out.println(binarySearch(haystack,"needle"));
+    }
+    public static int normalSearch(ArrayList<String> haystack, String needle){
+        for(int i = 0; i < haystack.size(); i++){
+            if(haystack.get(i).equals(needle)){
+                return i;
+            }
+            
+        }
+        return -1;
+    }
+
+    public static int binarySearch(ArrayList<String> haystack, String needle){
+        int size = haystack.size();
+        int start = 0;
+        int last = size - 1;
+        int middle = 0;
+
+        while(start <= last){
+            middle =  (start + last)/2;
+            if(haystack.get(middle).equals(needle)){
+                return middle;
+            }else if(haystack.get(middle).compareTo(needle) < 0){
+                start = middle + 1;
+            }else{
+                last = middle - 1;
+            }
+        }
+        return -1;
+    }
+    
+
+
+
+
+
+
+}
---
+++ answers.md
@@ -0,0 +1,9 @@
+1. I chose lego minifigures as my item. I found that once that particular item was found, it did not search the rest. This is just like a Where's Waldo book as once Waldo was found, the person immediately turns to the next page and never searched the rest of the page for the wizard or other characters. I also noticed that the binary algorithm doesn't have this issue and is a lot faster than searching the whole list.
+
+2. Binary search is a lot more effective because is doesn't have the possibility of having to search through the entire list to find the item. Instead it goes to roughly the middle of the list, see if it is to the left or right side of the middle point, and moves to whichever group holds the needle. It repeats this process until the needle is found.
+
+3a. To find "Blueberry" the normal search method would look through the entire list in order to find it.
+
+The binary search would first create a index for each item on the list then would split the list in half and check the lowest or highest half of the list and would keep going into the list cannot be split then check each item in the split and it would return if it is found. 
+3b. The search method accessed all of them, the binary search accessed 
+3c. If it was that long, the answer would stay the same.
